7961a0c24e3a8f0fafae2c13494aa430680f9144
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-02-11 18:59:17 +0530

    Added category post association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_131151) do
+ActiveRecord::Schema.define(version: 2020_02_11_132732) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_02_11_131151) do
     t.boolean "published"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id"
   end
 
   create_table "users", force: :cascade do |t|
