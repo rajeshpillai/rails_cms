4c2f41bd778294d334c537703892e9c0718ae88a
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-02-11 19:10:08 +0530

    Seed data and show category name in home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.destroy_all
+Category.destroy_all
+
+Category.create(title: "Electronics", description:"All about electronics")
+Category.create(title: "Books", description:"All about books")
+Category.create(title: "Video Tutorials", description:"Video tutorials")
+Category.create(title: "Programming", description:"All about programming")
+Category.create(title: "Web Development", description:"All about web development")
+
+
+5.times do |index|
+  offset = rand(Category.count)
+  category = Category.offset(offset).first
+  Post.create(title: "Post " + index.to_s, description: "Post goes here", published: true, category: category )
+end
+
+
+3.times do |index|
+  offset = rand(Category.count)
+  category = Category.offset(offset).first
+  Post.create(title: "Post " + index.to_s, description: "Post goes here", published: false, category: category )
+end
+
