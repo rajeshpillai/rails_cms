072d48cf1500306fb49c04965db03fdd3956c383
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-02-11 22:34:10 +0530

    Task: Link category from home page using category slug.

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,4 +2,12 @@ class PublicController < ApplicationController
   def index
     @posts = Post.includes(:category).where(published: true).order(created_at: :desc)
   end
+
+  def search
+    # @posts = Category.where(:title => params[:category_slug])
+    @posts = Post.select('posts.*')
+          .joins(:category)
+          .where('lower(categories.slug)=lower(?)',params[:category_slug])
+    render "public/index"
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ApplicationRecord
   has_many :posts
+  before_save :create_slug
+
+  validates  :title, presence: true
+  validates  :title, uniqueness: true
+
+  def create_slug
+    self.slug = self.title.downcase.tr(" ", "-")
+  end
 end
