eb1f63b05fe0e8b40cfaef8808d891c65acbddd0
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-02-11 22:51:25 +0530

    Link helper for creating conditional back link to public/index action

--- app/helpers/public_helper.rb
+++ app/helpers/public_helper.rb
@@ -1,2 +1,7 @@
 module PublicHelper
+  def back_link(params, name: "Back", className: "btn btn-primary")
+    if params[:category_slug].present?
+      link_to name, root_path, class: className 
+    end
+  end
 end
