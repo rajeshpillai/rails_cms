733553c81cd51a0a043ea0e95882c5c59cbc34be
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-02-11 18:36:37 +0530

    Post scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_104818) do
+ActiveRecord::Schema.define(version: 2020_02_11_130303) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.boolean "published"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
