012c54b08b3fc68e818cbf60d3c80edffc5efc3a
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-02-13 12:41:43 +0530

    Task: Create comment for post (non optimized)

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,4 +1,5 @@
 class PublicController < ApplicationController
+  before_action :authenticate_user!, only: [:comments]
   def index
     @posts = Post.includes(:category).where(published: true).order(created_at: :desc)
   end
@@ -20,5 +21,20 @@ class PublicController < ApplicationController
   def read
     @post = Post.find(params[:post_id])
     render "public/posts/read"
+  end
+
+  def comments
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.save
+
+    redirect_to post_read_path(@post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:post_id, :title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,13 @@ Rails.application.routes.draw do
     resources :posts
   end
 
+
   get 'public/category/:category_slug', to: "public#search", :as => :category_search
   get 'public/tags/:tag', to: "public#search", :as => :tag_search
   get 'public/posts/read/:post_id', to: "public#read", :as => :post_read
-
+  
+  post 'public/comments/:post_id', to: "public#comments", :as=>:comments
+  
   root 'public#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
